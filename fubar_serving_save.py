d2bfcbc1a45581f8790ad4d32b88bcfe948f4da7
Krzysztof J. Czarnecki <kjczarnecki@icloud.com>
2019-06-24 15:02:43 +0200

    Generalized fubar_serving_save.py

--- fubar_serving_save.py
+++ fubar_serving_save.py
@@ -2,8 +2,10 @@ import tensorflow as tf
 import json
 import os
 
-with open('model_partfreeze0.1.json', 'r') as f:
-    model = tf.keras.models.model_from_json(f.read())
-model.load_weights('weights_partfreeze0.1.h5')
+model_version = input('Enter model version to be exported as protobuf :')
 
-tf.saved_model.save(model, os.getcwd() + '/1')
+with open(f'model_partfreeze{model_version}.json', 'r') as f:
+    model = tf.keras.models.model_from_json(f.read())
+model.load_weights(f'weights_partfreeze{model_version}.h5')
+
+tf.saved_model.save(model, os.getcwd() + f'/{model_version}')
