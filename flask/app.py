47c8d88319df292fc13886bbc9d930574c1e0cfe
Fubar Server <kjczarne@gmail.com>
2019-06-27 01:13:09 +0000

    fubar web app

---
+++ flask/app.py
@@ -0,0 +1,86 @@
+
+
+
+import os
+from flask import Flask, jsonify, flash, request, redirect, url_for, render_template, send_from_directory
+from PIL import Image
+from werkzeug.utils import secure_filename
+import importlib.util
+# from image_processing_new import fubar_master_function
+spec = importlib.util.spec_from_file_location("fubar_master", "/home/ubuntu/fubar/image_processing_new.py")
+fubar_master = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(fubar_master)
+
+UPLOAD_FOLDER = '/home/ubuntu/fubar/flask/uploads'
+# UPLOAD_FOLDER = '/home/ubuntu/fubar/flask/static/uploads'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = b'-I\xd9I\xa0\xe7R\x83Q\xc0\xce\xf2\xe4\xc8\x020'
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg',])
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/")
+def front_page():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            drawpath = os.path.join(app.config['UPLOAD_FOLDER'], 'draw.jpg')
+            global pred
+            pred = fubar_master.fubar_master_function(path, outfile_draw=drawpath)
+            print(pred)
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    # else:
+    #     return redirect(url_for('front_page'))
+
+
+@app.route('/upload/<filename>')
+def send_file(filename):
+    # path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    # render_template("photo_sent.html", 
+    #             drawpath=path,
+    #             draw_image=
+    #         )
+
+
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return render_template('photo_sent.html', drawpath=path, filename=filename, pred=pred)
+
+@app.route('/data')
+def names():
+    data = {"names": ["John", "Jacob", "Julie", "Jennifer", "Satan"]}
+    return jsonify(data)
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(host='0.0.0.0')
+
---
+++ flask/fubar_REST.py
@@ -0,0 +1,42 @@
+# Adapted from https://gist.github.com/eavidan/07928337e2859bf8fa607f5693ee4a89#file-tensorflow_serving_rest_client-py
+
+from cnn_toolkit import image_bytestring, load_byte_img
+from fubar_CONF import hprm
+import numpy as np
+import requests
+
+
+def tf_serving_predict(image,
+                       host,
+                       port=8501,
+                       model_name='fubar',
+                       model_version='4',
+                       batch_size=1,
+                       signature_name='serving_default'):
+    """
+    function placing calls to the TF Serving Model through REST-API interface
+    :param image: path to inference image or image as np.array
+    :param host: host address of server with exposed API port
+    :param port: API port, default is 8501
+    :param model_name: string, model name, default is 'fubar'
+    :param model_version: integer, denotes currently served model version
+    :param batch_size: batch_size of photos, default is 1
+    :param signature_name: model signature_name
+    :return: JSON string result
+    """
+    if isinstance(image, str):
+        im = load_byte_img(image_bytestring(image), hprm['INPUT_H'], hprm['INPUT_W']) / 255
+    elif isinstance(image, np.ndarray):
+        im = image
+    else:
+        raise TypeError('Invalid image argument!')
+    batch = np.repeat(im, batch_size, axis=0).tolist()
+    request = {
+        "signature_name": signature_name,
+        "instances": batch
+    }
+    response = requests.post(f"http://{host}:{port}/v1/models/{model_name}/versions/{model_version}:predict",
+                             json=request)
+    # response = requests.post(f"http://localhost:8501/v1/models/fubar/versions/4:predict", json=request)
+    result = response.json()
+    return result
