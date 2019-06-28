26c3a0b1d84d008e78a83be8d80b87b47548c15a
Krzysztof J. Czarnecki <kjczarnecki@icloud.com>
2019-06-28 19:40:47 +0200

    improved patterns

--- benchmarks/bench_YOLO.py
+++ benchmarks/bench_YOLO.py
@@ -22,6 +22,23 @@ def fubar_benchmark_function(thresh_linspace_div=10, iou_thresh=0.5):
     cwd = os.getcwd()
     os.chdir(path_conf['yolo_darknet_app'])
     thresholds = np.linspace(0, 1, thresh_linspace_div)
+
+    patterns = {
+        'precision': r'(?<=precision = )\d\.\d+',
+        'recall': r'(?<=recall = )\d\.\d+',
+        'f1': r'(?<=F1-score = )\d\.\d+',
+        'TP': r'(?<=[^\(]TP = )\d+',
+        'FP': r'(?<=FP = )\d+(?<=,)',
+        'FN': r'(?<=[^\(]FN = )\d+',
+        'TP_per_class': r'(?<=\(TP = )\d+',
+        'FP_per_class': r'(?<=FP = )\d+(?=\))',
+        'ap_per_class': r'(?<=ap = )\d+\.\d+',
+        'iou_thresh': r'(?<=IoU threshold = )\d+',
+        'thresh': r'(?<=thresh = )\d+\.\d+',
+        'class_names': r'(?<=name = )\w+',
+        'map': r'(?<=\(mAP@\d\.\d+\) = )\d\.\d+'
+    }
+
     for i in thresholds:
         # result = subprocess.run(['./darknet',
         #                          'detector',
@@ -45,21 +62,7 @@ class_id = 1, name = rack, ap = 80.40%           (TP = 225, FP = 23)\
  mean average precision (mAP@0.50) = 0.836113, or 83.61 %\
 Total Detection Time: 33.000000 Seconds"
         # per-class TP, FP and NP are sorted 0 to n, where n is number of classes
-        patterns = {
-            'precision': r'precision = \d\.\d+',
-            'recall': r'recall = \d\.\d+',
-            'f1': r'F1-score = \d\.\d+',
-            'TP': r'[^\(]TP = \d+',
-            'FP': r'[^\(]FP = \d+',
-            'FN': r'[^\(]FN = \d+',
-            'TP_per_class': r'\(TP = \d+',
-            'FP_per_class': r'FP = \d+(?:\))',
-            'ap_per_class': r'ap = .\d+\.\d+',
-            'iou_thresh': r'IoU threshold = \d+',
-            'thresh': r'thresh = \d+\.\d+',
-            'class_names': r'name = .\w+',
-            'map': r'\(mAP@\d\.\d+\) = \d\.\d+'
-        }
+
         # re.findall()
         results = {k: re.findall(v, result) for k, v in patterns.items()}
     return results
