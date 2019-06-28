92ed928aed0c78af50fde36cca4fd85895a057c3
Krzysztof J. Czarnecki <kjczarnecki@icloud.com>
2019-06-29 00:59:39 +0200

    bug fixes

--- benchmarks/bench_YOLO.py
+++ benchmarks/bench_YOLO.py
@@ -139,13 +139,14 @@ Total Detection Time: 33.000000 Seconds"
                                        for tp in results['TP_']]
         results = {k: v for k, v in copy_results.items()}  # update results with the copy
         for k, v in results.items():
-            for idx, val in enumerate(v):  # [0, 1, 2, 3]
-                if k == 'thresh':
-                    copy_results[k] = val  # right now YOLO doesn't allow to separately manipulate thresholds
-                                           # for each class, so we just select the first element of the list
-                else:
-                    new_key = k + f'{results["class_names"][idx]}'
-                    copy_results[new_key] = val
+            if hasattr(v, '__iter__'):
+                for idx, val in enumerate(v):  # [0, 1, 2, 3]
+                    if k == 'thresh':
+                        copy_results[k] = val  # right now YOLO doesn't allow to separately manipulate thresholds
+                                               # for each class, so we just select the first element of the list
+                    else:
+                        new_key = k + f'{results["class_names"][idx]}'
+                        copy_results[new_key] = val
 
         runs_dict[i] = copy_results  # throw in results dict into dict collecting all the runs
 
