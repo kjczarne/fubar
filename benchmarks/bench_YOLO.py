117931a2bcb8391e7c92f45a4803283bc40dc363
Krzysztof J. Czarnecki <kjczarnecki@icloud.com>
2019-06-28 20:00:25 +0200

    exp

--- benchmarks/bench_YOLO.py
+++ benchmarks/bench_YOLO.py
@@ -28,7 +28,7 @@ def fubar_benchmark_function(thresh_linspace_div=10, iou_thresh=0.5):
         'recall': r'(?<=recall = )\d\.\d+',
         'f1': r'(?<=F1-score = )\d\.\d+',
         'TP': r'(?<=[^\(]TP = )\d+',
-        'FP': r'(?<=FP = )\d+(?<=,)',
+        'FP': r'(?<=FP = )\d+(?=,)',
         'FN': r'(?<=[^\(]FN = )\d+',
         'TP_per_class': r'(?<=\(TP = )\d+',
         'FP_per_class': r'(?<=FP = )\d+(?=\))',
@@ -36,7 +36,7 @@ def fubar_benchmark_function(thresh_linspace_div=10, iou_thresh=0.5):
         'iou_thresh': r'(?<=IoU threshold = )\d+',
         'thresh': r'(?<=thresh = )\d+\.\d+',
         'class_names': r'(?<=name = )\w+',
-        'map': r'(?<=\(mAP@\d\.\d+\) = )\d\.\d+'
+        'map': r'(?<=\(mAP@\d.\d\d\) = )\d\.\d+'
     }
 
     for i in thresholds:
