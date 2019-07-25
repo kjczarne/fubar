9ad8b13ef11016fab7849f853a03d38e18b86cf6
Krzysztof J. Czarnecki <kjczarnecki@icloud.com>
2019-07-25 19:15:46 +0200

    fixed bug in fubar_serving_save.py

--- fubar_serving_save.py
+++ fubar_serving_save.py
@@ -3,8 +3,8 @@ import os
 
 model_version = input('Enter model version to be exported as protobuf :')
 
-with open(f'model_partfreeze{model_version}.json', 'r') as f:
+with open(f'model_partfreeze_{model_version}.json', 'r') as f:
     model = tf.keras.models.model_from_json(f.read())
-model.load_weights(f'weights_partfreeze{model_version}.h5')
+model.load_weights(f'weights_partfreeze_{model_version}.h5')
 
 tf.saved_model.save(model, os.getcwd() + f'/{model_version}')
